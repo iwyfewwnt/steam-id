4b67d17015b0fe4143d866240f14322dc712c863
iwyfewwnt <tied10v3@mail.ru>
2023-03-15 14:10:10 +0300

    Rename package groupId to "io.github.iwyfewwnt"

---
+++ src/main/java/io/github/iwyfewwnt/steamid/utils/USteamDomain.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2023 u004
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.github.iwyfewwnt.steamid.utils;
+
+/**
+ * A Steam domains utility.
+ *
+ * <p>{@code USteamDomain} is the utility class
+ * for Steam's domains that were used in this project.
+ */
+public final class USteamDomain {
+
+	/**
+	 * A worldwide Steam community domain string.
+	 */
+	public static final String COMMUNITY = "steamcommunity.com";
+
+	/**
+	 * A Steam invite domain string.
+	 */
+	public static final String INVITE = "s.team";
+
+	/**
+	 * A Steam China community domain string.
+	 */
+	public static final String CHINA = "my.steamchina.com";
+
+	private USteamDomain() {
+		throw new UnsupportedOperationException();
+	}
+}
---
+++ src/main/java/io/github/iwyfewwnt/steamid/utils/USteamPattern.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2023 u004
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.github.iwyfewwnt.steamid.utils;
+
+import java.util.regex.Pattern;
+
+/**
+ * A Steam patterns utility.
+ *
+ * <p>{@code USteamPattern} is the utility class
+ * for compiled {@link USteamRegex}'s regular expressions.
+ */
+public final class USteamPattern {
+
+	/**
+	 * A Steam ID2 pattern.
+	 *
+	 * <p>Wraps {@link USteamRegex#ID2}.
+	 */
+	public static final Pattern ID2 = Pattern.compile(USteamRegex.ID2);
+
+	/**
+	 * A Steam ID3 pattern.
+	 *
+	 * <p>Wraps {@link USteamRegex#ID3}.
+	 */
+	public static final Pattern ID3 = Pattern.compile(USteamRegex.ID3);
+
+	/**
+	 * A Steam profile url pattern.
+	 *
+	 * <p>Wraps {@link USteamRegex#PROFILE_URL}.
+	 */
+	public static final Pattern PROFILE_URL = Pattern.compile(USteamRegex.PROFILE_URL);
+
+	/**
+	 * A Steam user url pattern.
+	 *
+	 * <p>Wraps {@link USteamRegex#USER_URL}.
+	 */
+	public static final Pattern USER_URL = Pattern.compile(USteamRegex.USER_URL);
+
+	private USteamPattern() {
+		throw new UnsupportedOperationException();
+	}
+}
