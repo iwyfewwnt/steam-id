16369cee91b62df5cba7d140b74190c2cbe0dd82
iwyfewwnt <tied10v3@mail.ru>
2023-03-18 11:53:30 +0300

    Fix USteamInvite#XUID_BASE doc-comment

--- src/main/java/io/github/iwyfewwnt/steamid/utils/USteamInvite.java
+++ src/main/java/io/github/iwyfewwnt/steamid/utils/USteamInvite.java
@@ -42,7 +42,7 @@ public final class USteamInvite {
 	public static final String MAX_CODE = "wwww-wwww";
 
 	/**
-	 * A Steam xuid base.
+	 * A Steam unique account identifier base.
 	 */
 	public static final String XUID_BASE = "0123456789abcdef";
 
